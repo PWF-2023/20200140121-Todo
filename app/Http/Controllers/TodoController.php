8ea7e20e462a0518bb33e0676d7850cbb980d3c9
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2023-04-14 14:27:39 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,11 +9,39 @@ class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();    
 
-        dd($todos);
+        // dd($todos);
+        return view('todo.index', compact('todos'));
+    }
 
-        return view('todo.index');
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        //Practical
+        //$todo = new Todo;
+        //$todo->title = $request->title;
+        //$todo->user_id = auth()->user()->id;
+        //$todo->save();
+
+        //Query Builder Way
+        //DB::table('todos')->insert([
+        //'title' => $request->title,
+        //'user_id' => auth()->user()->id,
+        //'created_at' => now(),
+        //'updated_at' => now(),
+        //])
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
     }
 
     public function create()
---
+++ tugas5.md
@@ -0,0 +1,10 @@
+#  Tugas 5
+
+## 1. Todo Create
+![Alt text](screenshot/tugas5/2023-04-14 (1).png)
+## 2. Todo Created
+![Alt text](screenshot/tugas5/2023-04-14 (2).png)
+## 3. User Search
+![Alt text](screenshot/tugas5/2023-04-14 (3).png)
+## 4. User Search Result
+![Alt text](screenshot/tugas5/2023-04-14 (4).png)
